845abd26d71996a1925abb46f68258c619d7a9b5
yseij <r0790156@student.thomasmore.be>
2022-06-21 11:58:29 +0200

    start

---
+++ src/main/java/com/example/quidsolutionsproject/QuidSolutionsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.quidsolutionsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuidSolutionsProjectApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(QuidSolutionsProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/quidsolutionsproject/QuidSolutionsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quidsolutionsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuidSolutionsProjectApplicationTests {
+
+    @Test
+    void contextLoads(){
+    }
+
+}
